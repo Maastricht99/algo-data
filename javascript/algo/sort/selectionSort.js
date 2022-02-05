5f9f2bd7025d3861ef07fa7992d2eb39130969ff
Alex <19alexferrante99@gmail.com>
2022-02-05 23:10:25 +0100

    add selection sort

---
+++ javascript/algo/sort/selectionSort.js
@@ -0,0 +1,15 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    const temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
+  }
+  return arr;
+}
+
