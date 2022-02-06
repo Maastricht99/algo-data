42e537c39d397f3c6b3496e5a9cf45ebae37f5b7
Alex <19alexferrante99@gmail.com>
2022-02-06 16:07:10 +0100

    add get digit count helpers for radix sort

--- javascript/algo/sort/radixSort.js
+++ javascript/algo/sort/radixSort.js
@@ -8,3 +8,12 @@ function getDigitFormula(number, place) {
   return Math.floor(Math.abs(number) / Math.pow(10, place)) % 10;
 }
 
+function digitCount(number) {
+  const strNumber = number.toString();
+  return strNumber.replace("-", "").length;
+}
+
+function digitCountFormula(number) {
+  return Math.floor(Math.log10(Math.abs(number))) + 1;
+}
+
