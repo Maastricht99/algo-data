31a196a47f83ae45fd4c55862fdb28caea429a0c
Alex <19alexferrante99@gmail.com>
2022-02-10 20:13:50 +0100

    add graph

---
+++ data/graph.js
@@ -0,0 +1,42 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+  removeVertex(vertex) {
+    if (this.adjacencyList[vertex]) {
+      for (let i = 0; i < this.adjacencyList[vertex].length; i++) {
+        const vertex2 = this.adjacencyList[vertex][i];
+        for (let j = 0; j < this.adjacencyList[vertex2].length; j++) {
+          if (this.adjacencyList[vertex2][j] === vertex) {
+            this.adjacencyList[vertex2].splice(j, 1);
+          }
+        }
+      }
+      delete this.adjacencyList[vertex];
+    }
+  }
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+    }
+  }
+  removeEdge(vertex1, vertex2) {
+    for (let i = 0; i < this.adjacencyList[vertex1].length; i++) {
+      if (this.adjacencyList[vertex1][i] === vertex2) {
+        this.adjacencyList[vertex1].splice(i, 1);
+      }
+    }
+    for (let i = 0; i < this.adjacencyList[vertex2].length; i++) {
+      if (this.adjacencyList[vertex2][i] === vertex1) {
+        this.adjacencyList[vertex2].splice(i, 1);
+      }
+    }
+  }
+}
+
