4db773c04de87b735a76523505d68832949aa081
Alex <19alexferrante99@gmail.com>
2022-02-10 02:16:43 +0100

    add extract max method binary heap

--- data/maxBinaryHeap.js
+++ data/maxBinaryHeap.js
@@ -17,5 +17,42 @@ class MaxBinaryHeap {
     }
     return;
   }
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0){
+      this.values[0] = end;
+      let index = 0;
+      const length = this.values.length;
+      const element = this.values[0];
+      while (true) {
+        let leftIndex = 2 * index + 1;
+        let rightIndex = 2 * index + 2;
+        let leftChild;
+        let rightChild;
+        let swap = null;
+        if (leftIndex < length) {
+          leftChild = this.values[leftIndex];
+          if (leftChild > element) {
+            swap = leftIndex;
+          }
+        }
+        if (rightIndex < length) {
+          rightChild = this.values[rightIndex];
+          if ((!swap && rightChild > element) || 
+            (swap && rightChild > leftChild)) {
+            swap = rightIndex;
+          }
+        }       
+        if (swap) {
+          this.values[index] = this.values[swap];
+          this.values[swap] = element;
+          index = swap;
+        } else {
+          break;
+        }
+      }
+    }
+    return max;
+  }
 }
-
