26a1cbb1c22ebdf2efc2ac4f2620f7536c18f939
Alex <19alexferrante99@gmail.com>
2022-02-13 20:35:43 +0100

    add priority queue to graph

--- data/weightedGraph.js
+++ data/weightedGraph.js
@@ -1,3 +1,19 @@
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(value, priority) {
+    this.values.push({ value, priority });
+    this.sort();
+  }
+  dequeue() {
+    return this.values.shift();
+  }
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
+
 class Graph {
   constructor() {
     this.adjacencyList = {};
