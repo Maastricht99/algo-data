9fba53c75cc9b5c78d2573709522e684b9051dac
Alex <19alexferrante99@gmail.com>
2022-02-13 22:29:41 +0100

    add Dijkstra to weighted graph

--- data/weightedGraph.js
+++ data/weightedGraph.js
@@ -29,5 +29,41 @@ class Graph {
       this.adjacencyList[vertex2].push({ value: vertex1, weight });
     }
   }
+  Dijkstra(start, target) {
+    const distances = {};
+    const previous = {};
+    const path = [];
+    const priorityQueue = new PriorityQueue();
+    const vertices = Object.keys(this.adjacencyList);
+    for (let i = 0; i < vertices.length; i++) {
+      const vertex = vertices[i];
+      previous[vertex] = null;
+      if (vertices[i] == start) {
+        distances[vertex] = 0;
+        priorityQueue.enqueue( vertex, 0 );
+      } else {
+        distances[vertex] = Infinity;
+        priorityQueue.enqueue( vertex, Infinity );
+      }
+    }
+    while (priorityQueue.values.length) {
+      const vertex = priorityQueue.dequeue();
+      if (vertex.value == target) {
+        while (previous[vertex.value]) {
+          path.push(vertex.value);
+          vertex.value = previous[vertex.value];
+        }
+        return path.reverse();
+      }
+      for (let i = 0; i < this.adjacencyList[vertex.value].length; i++) {
+        const adjacentVertex = this.adjacencyList[vertex.value][i];
+        const newDistance = distances[vertex.value] + adjacentVertex.weight;
+        if (newDistance < distances[adjacentVertex.value]) {
+          distances[adjacentVertex.value] = newDistance;
+          previous[adjacentVertex.value] = vertex.value;
+          priorityQueue.enqueue( adjacentVertex.value, newDistance );
+        }
+      }
+    }
+  }
 }
-
