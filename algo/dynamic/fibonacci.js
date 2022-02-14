5942440724b68c6dffa5d3da9c4615519686882e
Alex <19alexferrante99@gmail.com>
2022-02-14 14:27:31 +0100

    add memoization and tabulation

--- algo/dynamic/fibonacci.js
+++ algo/dynamic/fibonacci.js
@@ -1,19 +1,29 @@
-function fibonacciRecursive(n) {
+function fibRecursive(n) {
   if (n <= 2) {
     return 1;
   }
-  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+  return fibRecursive(n - 1) + fibRecursive(n - 2);
 }
 
-function fibonacciDynamic(n, memo = {}) {
+function fibMemo(n, memo = {}) {
   if (memo[n]) {
     return memo[n];
   }
   if (n <= 2) {
     return 1;
   }
-  const res = fibonacciDynamic(n - 1, memo) + fibonacciDynamic(n - 2, memo);
+  const res = fibMemo(n - 1, memo) + fibMemo(n - 2, memo);
   memo[n] = res;
   return res;
 }
 
+function fibTable(n) {
+  if (n <= 2) {
+    return 1;
+  }
+  const fibNumbers = [0, 1, 1];
+  for (let i = 3; i <= n; i++) {
+    fibNumbers[i] = fibNumbers[i - 1] + fibNumbers[i - 2];
+  }
+  return fibNumbers[n];
+}
